7972a419ae3fafe5185c814fd949c81bfdab9930
darkneesheart <mylegin4115@gmail.com>
2023-11-11 23:34:03 +0330

    hello world

---
+++ ave.cpp
@@ -0,0 +1,15 @@
+#include<stdfix.h>
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{float i,x,sum,ave;
+    for (i=0;i<=10;i++)
+    {
+     cin>>x;
+     sum=sum+x;
+    }
+    ave=sum/10;
+    cout<<"javab="<<sum;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<stdfix.h>
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{float i,x,sum;
+    for (i=0;i<=10;i++)
+    {
+     cin>>x;
+     sum=sum+x;
+    }
+    cout<<"javab="<<sum;
+    return 0;
+}
