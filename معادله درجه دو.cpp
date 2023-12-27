c8eb1e3cbfe87d979a0fe9721a8f340dd1a028a6
darkneesheart <150263011+darkneesheart@users.noreply.github.com>
2023-12-27 14:21:11 +0330

    Update معادله درجه دو.cpp

--- معادله درجه دو.cpp
+++ معادله درجه دو.cpp
@@ -1,30 +1,18 @@
-#include <iostream>
+#include<iostream>
 #include<cmath>
 using namespace std;
-
-int main() 
-{
-    float a,b,c,x1,x2,x3,delta;
-
-    cout <<"Enter three numbers: ";
-    cin >>a>>b>>c;
-    delta=(b*b-4*(a*c));
-    cout<<"delta="<<delta<<endl;
-     
-
-    
-    if (delta<0)
-      cout<<"no answer";
-      
-       else if(delta=0)
-      {
-       (x1=(-b/(2*a)));
-       cout<<endl<<x1;
-      }
-         else {x2=(-b+sqrt(delta))/(2*a));
-               x3=(-b-sqrt(delta))/(2*a));
-                cout<<endl<<x1<<endl<<x2;}          
+int main()
+{double a,b,c,d,x1,x2;
+    cin>>a>>b>>c;
+    d =(b*b)-(4*a*c);
+    cout<<"Delta= "<<d<<endl;
+    if(d<0)
+    cout<<"not";
+    else{
+        x1=(-b+sqrt(d))/(2*a);
+        x2=(-b-sqrt(d))/(2*a);
+        cout<<"x1= "<<x1<<endl<<"x2= "<<x2;
+        }
+   
     return 0;
-    cin.get();
-    cin.get();
 }
