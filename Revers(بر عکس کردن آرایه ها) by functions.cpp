aa3f07f40777c65b7ee1888553604c29d9ce3fb0
darkneesheart <150263011+darkneesheart@users.noreply.github.com>
2023-12-27 19:34:07 +0330

    Update Revers(بر عکس کردن آرایه ها) by functions.cpp

--- Revers(بر عکس کردن آرایه ها) by functions.cpp
+++ Revers(بر عکس کردن آرایه ها) by functions.cpp
@@ -1,18 +1,22 @@
   #include<stdafx.h>
 #include<iostream>
 using namespace std;
-void output(int A[])
-{for (int i=0;i<10;i++)
-cout<<A[i];}
+//first function *******
 void input(int A[]){
     for(int i=0;i<10;i++)
     cin>>A[i];}
+//second function ******
+void output(int A[])
+     {for (int i=0;i<10;i++)
+     cout<<A[i];}
+//third function ******
 void Rev(int A[],int B[])
 {
     int i,j=9;
     for(i=0;i<10;i++)
     B[j--]=A[i];
     }
+//main function *******
 int main()
 {
     int A[10],B[10];
